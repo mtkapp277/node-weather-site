1d2c6d9c23a6a71260e4784c2d6aab22cd0a48be
Michael Kapp <miketkapp@gmail.com>
2020-06-25 17:20:48 -0400

    Initial Commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('postman-request')
+
+const forecast = (longitude, latitiude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=625f0806e4c7ba09865264eaa6dbce73&query=' + latitiude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            console.log('mk: ' + error)
+            callback('Unable to find location', undefined)
+        } else {       
+            const temp = body.current.temperature
+            const feels = body.current.feelslike
+            const desc = body.current.weather_descriptions[0]
+            //console.log(desc + '. It is currently ' + temp + ' degrees out. It feels like ' + feels + ' degrees out.')
+            callback(undefined, desc + '. It is currently ' + temp + ' degrees out. It feels like ' + feels + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
+
+
---
+++ src/utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require('postman-request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibXRrYXBwMjc3IiwiYSI6ImNrMmNiamJqNTF1ZW8zY2xzZThyOXBuYW8ifQ.FsMCRxL0cc2ttpPWHYGBfg&limit=1'
+    
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            const latitude = body.features[0].center[1]
+            const longitude = body.features[0].center[0]
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
